5089548c7e6660d308a3f7f89869432bf7a74ab5
dmccullor <dmccullor0@gmail.com>
2022-02-15 21:43:44 -0600

    converted reading code block into function

--- RC_poll.py
+++ RC_poll.py
@@ -20,11 +20,6 @@ file_to_load = os.path.join("Resources", "rc_election_results.csv")
 file_to_save = os.path.join("rc_election_analysis.txt")
 
 
-#Initialize a total vote counter.
-total_votes = 0
-#Candidate optioins and candidate votes
-candidate_options = []
-candidate_votes = {}
 
 #Track the winning candidate, vote count and percentage.
 winning_candidate = ""
@@ -32,28 +27,37 @@ winning_count = 0
 winning_percentage = 0
 
 
-# Open the election results and read the file.
-with open(file_to_load, 'r') as election_data:
-    
-    #Read the file object with the reader function.
-    file_reader = csv.reader(election_data)
-    #Read the header row.
-    headers = next(file_reader)
-    #Print each row in the CSV file.
-    for row in file_reader:
-        #Add to the total vote count.
-        total_votes += 1
-        #Get the candidate name for each row.
-        candidate_name = row[2]
+def vote_retrieval():
+    #Initialize a total vote counter.
+    total_votes = 0
+    #Candidate options and candidate votes
+    candidate_options = []
+    candidate_votes = {}
+    # Open the election results and read the file.
+    with open(file_to_load, 'r') as election_data:
+        #Read the file object with the reader function.
+        file_reader = csv.reader(election_data)
+        #Read the header row.
+        headers = next(file_reader)
+        #Print each row in the CSV file.
+        for row in file_reader:
+            #Add to the total vote count.
+            total_votes += 1
+            #Get the candidate name for each row.
+            candidate_name = row[2]
 
-        #If the candidate does not match any existing cnadidate add it to the candidate list.
-        if candidate_name not in candidate_options:
-            #Add the candidate name to the candidate list.
-            candidate_options.append(candidate_name)
-            #And begin tracking that candidate's voter count.
-            candidate_votes[candidate_name] = 0
-        #Add a vote to that candidate's count
-        candidate_votes[candidate_name] += 1
+            #If the candidate does not match any existing cnadidate add it to the candidate list.
+            if candidate_name not in candidate_options:
+                #Add the candidate name to the candidate list.
+                candidate_options.append(candidate_name)
+                #And begin tracking that candidate's voter count.
+                candidate_votes[candidate_name] = 0
+            #Add a vote to that candidate's count
+            candidate_votes[candidate_name] += 1
+    
+    return total_votes, candidate_votes
+
+total_votes, candidate_votes = vote_retrieval()
 
 with open(file_to_save, "w") as txt_file:
     #Print the final vote count to the terminal.

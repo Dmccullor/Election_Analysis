ade80d001cc5c77519f52044d4bb0c85a0d15046
dmccullor <dmccullor0@gmail.com>
2022-02-18 00:16:38 -0600

    Prototype RC vote script complete

--- RC_poll.py
+++ RC_poll.py
@@ -10,10 +10,6 @@ import csv
 import os
 
 # Assign a variable for the file to load and the path.
-#Windows only file_to_load below
-#file_to_load = "D:\Class Docs\Data Analysis Projects\Election_Analysis\Resources\election_results.csv"
-#Mac only file_to_load below
-#file_to_load = "/Users/dmccu/Documents/Class Docs/Analysis Projects/Election_Analysis/Resources/election_results.csv"
 file_to_load = os.path.join("Resources", "rc_election_results.csv")
 
 #Assign a variable to save the file to a path.
@@ -21,13 +17,18 @@ file_to_save = os.path.join("rc_election_analysis.txt")
 
 
 
-#Track the winning candidate, vote count and percentage.
+#Track the winning candidate, vote count and percentage as well as eliminated candidate, eliminated candidate list and lowest percentage.
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
+eliminated_candidates = []
+eliminated_candidate = ""
+lowest_percentage = 0
 
+#Initiate round counter
+round = 0
 
-def vote_retrieval(*candidates):
+def vote_retrieval(*eliminated_candidates, round):
     #Initialize a total vote counter.
     total_votes = 0
     #Candidate options and candidate votes
@@ -44,7 +45,10 @@ def vote_retrieval(*candidates):
             #Add to the total vote count.
             total_votes += 1
             #Get the candidate name for each row.
-            candidate_name = row[2]
+            if row[2] in eliminated_candidates:
+                candidate_name = row[2 + round]
+            else:
+                candidate_name = row[2]
 
             #If the candidate does not match any existing cnadidate add it to the candidate list.
             if candidate_name not in candidate_options:
@@ -55,24 +59,43 @@ def vote_retrieval(*candidates):
             #Add a vote to that candidate's count
             candidate_votes[candidate_name] += 1
     
-    return total_votes, candidate_votes
+    return total_votes, candidate_votes, candidate_options
 
-total_votes, candidate_votes = vote_retrieval()
+total_votes, candidate_votes, candidate_options = vote_retrieval()
 
-eliminated_candidates = []
-election_summary = {}
+def winner_calculation(total_votes, candidate_votes):
+    for candidate_name in candidate_votes:
+        #Retrieve vote count and percentage.
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        #Determine winning vote count, winning percentage and candidate.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
 
-for candidate_name in candidate_votes:
-    #Retrieve vote count and percentage.
-    votes = candidate_votes[candidate_name]
-    vote_percentage = float(votes) / float(total_votes) * 100
-    election_summary.setdefault(candidate_name, []).append(votes)
-    election_summary.setdefault(candidate_name, []).append(vote_percentage)
-    #Determine winning vote count, winning percentage and candidate.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
+        if lowest_percentage == 0 and vote_percentage >= 0:
+            eliminated_candidate = candidate_name
+            lowest_percentage = vote_percentage
+        elif vote_percentage < lowest_percentage:
+            eliminated_candidate = candidate_name
+            lowest_percentage = vote_percentage    
+        else:
+            pass
+    
+    return winning_count, winning_percentage, winning_candidate, eliminated_candidate
+
+
+#Loop through candidates
+#Add +1 to round counter after each round
+#Add bottom percentage candidatte to eliminated candidates
+for i in len(candidate_options):
+    total_votes, candidate_votes, candidate_options = vote_retrieval(eliminated_candidates, round)
+    winning_count, winning_percentage, winning_candidate = winner_calculation(total_votes, candidate_votes)
+    eliminated_candidates.append(eliminated_candidates)
+    round +=1
+    if winning_percentage > 50:
+        break
 
 with open(file_to_save, "w") as txt_file:
     #Print the final vote count to the terminal.
@@ -85,9 +108,11 @@ with open(file_to_save, "w") as txt_file:
     #Save the final vote count to the text file.
     txt_file.write(election_results)
 
-    for candidate in election_summary:
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
         #Print each candidate, their voter count, and percentage to the terminal.
-        candidate_results = (f"{candidate}: {election_summary[candidate][1]:.1f}% ({election_summary[candidate][0]:,} votes)\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,} votes)\n")
         print(candidate_results)
         #Save the candidate results to our text file.
         #txt_file.write(candidate_results)
